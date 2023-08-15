05443eef5b9ed3c20b1a3eefbf4bf88db096cf2d
elanonc <nunes.elano14@gmail.com>
2023-08-14 21:58:52 -0300

    API: GET by name and GET greater than value

--- src/main/java/com/example/springboot/controllers/ProductController.java
+++ src/main/java/com/example/springboot/controllers/ProductController.java
@@ -10,9 +10,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class ProductController {
@@ -29,7 +33,14 @@ public class ProductController {
 
     @GetMapping("/products")
     public ResponseEntity<List<ProductModel>> getAllProducts(){
-        return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
+        List<ProductModel> productsList = productRepository.findAll();
+        if (!productsList.isEmpty()){
+            for(ProductModel product : productsList){
+                UUID id = product.getIdProduct();
+                product.add(linkTo(methodOn(ProductController.class).getOneProduct(id)).withSelfRel());
+            }
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(productsList);
     }
 
     @GetMapping("/products/{id}")
@@ -38,7 +49,23 @@ public class ProductController {
         if(productO.isEmpty()){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
         }
+        productO.get().add(linkTo(methodOn(ProductController.class).getOneProduct(id)).withRel("Products List"));
         return ResponseEntity.status(HttpStatus.OK).body(productO.get());
+    }
+
+    @GetMapping("/products/search")
+    public ResponseEntity<List<ProductModel>> getProductsByName(@RequestParam String name) {
+        List<ProductModel> productsList = productRepository.findByName(name);
+        if (productsList.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(productsList);
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(productsList);
+    }
+
+    @GetMapping("/products/value-greater-than")
+    public ResponseEntity<List<ProductModel>> getProductsByValueGreaterThan(@RequestParam BigDecimal value) {
+        List<ProductModel> products = productRepository.findByValueGreaterThan(value);
+        return ResponseEntity.status(HttpStatus.OK).body(products);
     }
 
     @PutMapping("/products/{id}")
@@ -60,6 +87,6 @@ public class ProductController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
         }
         productRepository.delete(productO.get());
-        return ResponseEntity.status(HttpStatus.OK).body("Product deleted successfully");
+        return ResponseEntity.status(HttpStatus.OK).body("Product deleted successfully.");
     }
 }
--- src/main/java/com/example/springboot/repositories/ProductRepository.java
+++ src/main/java/com/example/springboot/repositories/ProductRepository.java
@@ -4,9 +4,12 @@ import com.example.springboot.models.ProductModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
-
+    List<ProductModel> findByName(String name);
+    List<ProductModel> findByValueGreaterThan(BigDecimal value);
 }
